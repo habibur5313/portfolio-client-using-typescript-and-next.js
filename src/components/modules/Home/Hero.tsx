7380580440592e3490be3237597bb7a5da625339
habibur5313 <habiburmamun313@gmail.com>
2025-10-02 00:02:21 -0600

    downdload resume button added

--- src/components/modules/Home/Hero.tsx
+++ src/components/modules/Home/Hero.tsx
@@ -15,20 +15,28 @@ export default async function Hero() {
 
         <section className="relative flex flex-col items-center justify-center text-center py-28 px-6 text-white">
           {/* Headline */}
-          <h1 className="text-5xl md:text-7xl font-extrabold tracking-tight max-w-5xl leading-tight">
-            Build. Code. Inspire.
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold tracking-tight max-w-5xl leading-tight">
+            💻 Let’s Build the Future of Web Together
           </h1>
 
           {/* Subheadline */}
           <p className="mt-6 text-lg md:text-xl max-w-2xl">
-            Hi, I’m Habibur. I create dynamic web experiences using React, Node.js, and MongoDB. Explore my projects, tutorials, and ideas that push the boundaries of web development.
+            Hi, I’m Habibur Rahman. A Passionate Web Developer from Sylhet,
+            Bangladesh. I build modern, responsive, and user-friendly web
+            applications using MERN, Next.js, Prisma & more.
           </p>
 
           {/* CTA */}
-          <div className="mt-10 flex flex-col sm:flex-row gap-4">
+          <div className="mt-10 flex">
+            <Link
+              href="https://drive.usercontent.google.com/u/0/uc?id=1rub4hCa8zayvcy0Z8xjnmXFNPgvAYQqi&export=download"
+              className="inline-flex items-center justify-center px-4 py-2 sm:px-6 sm:py-3 font-medium rounded-l-xl border border-input hover:bg-accent hover:text-accent-foreground transition"
+            >
+              Download Resume
+            </Link>
             <Link
               href="/projects"
-              className="inline-flex items-center justify-center px-8 py-4 font-medium rounded-xl border border-input hover:bg-accent hover:text-accent-foreground transition"
+              className="inline-flex items-center justify-center px-4 py-2 sm:px-6 sm:py-3 font-medium rounded-r-xl border border-input hover:bg-accent hover:text-accent-foreground transition"
             >
               Explore Projects
             </Link>
